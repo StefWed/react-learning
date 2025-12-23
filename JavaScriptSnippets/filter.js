01b6f3e7742ef4abaa9917210fdf2c7b41b239d5
Stefanie Wedel <wedel.stefanie@protonmail.com>
2025-12-23 19:16:07 +0000

    Add Example of Mixed Filter and Map Plus Exercise

--- JavaScriptSnippets/filter.js
+++ JavaScriptSnippets/filter.js
@@ -6,3 +6,9 @@ const books = [
 const longBooks = books.filter(book => book.pages > 400)
 
 console.log(longBooks)
+
+const longTitle = books
+  .filter(b => b.pages >400)
+  .map(b => b.title)
+
+console.log(longTitle)
